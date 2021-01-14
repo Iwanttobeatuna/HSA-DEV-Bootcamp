4febf08148cbe29c2351155c064618f36d4760b2
Zad Chin <70809099+Iwanttobeatuna@users.noreply.github.com>
2021-01-14 21:04:33 +0800

    Add files via upload

---
+++ week1/1_PM/madlibs.js
@@ -0,0 +1,19 @@
+/* In this task, make a madlibs templates story
+using JS string template literals. In the story
+variable, write out the template of your madlibs
+using string variables in the place of certain
+nouns, adjectives, and verbs */
+
+/* Variables, i.e.
+const noun1 = ______
+const verb1 = ______
+
+Change thses values to generate a different story
+*/
+const noun1 = "van"
+const verb1 = "crash"
+const person1 = "person"
+const adj1 = "sad"
+const story = ` On a sunny day morning, ${person1} ${verb1} ${noun1}. That was ${adj1}. `
+
+console.log(story)
